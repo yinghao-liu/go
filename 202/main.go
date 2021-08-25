8b9f6dfbfae9ab09e79c1a96f0e32f6f5891ca50
francis <yinghao_liu@163.com>
2021-08-25 14:26:12 +0800

    gin routes

--- 202/main.go
+++ 202/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 1.创建路由
-	r := gin.Default()
-	// 2.绑定路由规则，执行的函数
-	// gin.Context，封装了request和response
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello World!")
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.StaticFile("/", "web/index.html")
+	router.StaticFile("/favicon.ico", "web/favicon.ico")
+	router.Static("/static", "resource")
+	router.POST("/upload", func(c *gin.Context) {
+		// Multipart form
+		form, _ := c.MultipartForm()
+		log.Println(form)
+		files := form.File["files"] // the input tag name attribute
+
+		for _, file := range files {
+			log.Println(file.Filename)
+			log.Println(file.Header)
+			log.Println(file.Size)
+
+			// Upload the file to specific dst.
+			c.SaveUploadedFile(file, "resource"+file.Filename)
+		}
+		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
 	})
-	// 3.监听端口，默认在8080
-	// Run("里面不指定端口号默认为8080")
-	r.Run(":8000")
+	router.Run(":8080")
 }
