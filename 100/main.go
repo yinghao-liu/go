319e5e05a59089f80b1d7fbb86238824ccb6d506
francis <yinghao_liu@163.com>
2021-11-02 17:27:18 +0800

    url

--- 100/main.go
+++ 100/main.go
@@ -5,10 +5,21 @@ import (
 	"net/url"
 )
 
+func urlGenerate() {
+	var a url.URL
+	a.Scheme = "http"
+	a.Host = "127.0.0.1:8000"
+	a.Path = "/v1/account"
+	fmt.Printf("url is %s\n", a.String())
+
+}
+
 func main() {
 	uoo := url.PathEscape("<dasdsd>/sdfsfdsdfs")
 	fmt.Println(uoo)
 
 	url2, ok := url.PathUnescape("/%3CplatformUrl%3E/allNotify/")
 	fmt.Println(url2, ok)
+
+	urlGenerate()
 }
