2e45c0f50ca7c6e5b39ff503cf563030f1d24ecc
francis <yinghao_liu@163.com>
2021-11-16 19:00:34 +0800

    杂项

--- 100/main.go
+++ 100/main.go
@@ -9,13 +9,15 @@ func urlGenerate() {
 	var a url.URL
 	a.Scheme = "http"
 	a.Host = "127.0.0.1:8000"
-	a.Path = "/v1/account"
+	//a.Path = "/v1/account?a=b"
+	a.RawPath = url.PathEscape("/v1/account?a=b")
+	//a.RawQuery = "a=b"
 	fmt.Printf("url is %s\n", a.String())
 
 }
 
 func main() {
-	uoo := url.PathEscape("<dasdsd>/sdfsfdsdfs")
+	uoo := url.PathEscape("/v1/account?a=b")
 	fmt.Println(uoo)
 
 	url2, ok := url.PathUnescape("/%3CplatformUrl%3E/allNotify/")
--- 102/main.go
+++ 102/main.go
@@ -4,8 +4,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 )
 
 type Address struct {
@@ -21,22 +19,60 @@ type VCard struct {
 	Remark    string
 }
 
+func parserTypeswitches() {
+
+}
+
 func main() {
-	pa := &Address{"private", "Aartselaar", "Belgium"}
-	wa := &Address{"work", "Boom", "Belgium"}
-	vc := VCard{"Jan", "Kersschot", []*Address{pa, wa}, "none"}
-	// fmt.Printf("%v: \n", vc) // {Jan Kersschot [0x126d2b80 0x126d2be0] none}:
+	// pa := &Address{"private", "Aartselaar", "Belgium"}
+	// wa := &Address{"work", "Boom", "Belgium"}
+	// vc := VCard{"Jan", "Kersschot", []*Address{pa, wa}, "none"}
+	// // fmt.Printf("%v: \n", vc) // {Jan Kersschot [0x126d2b80 0x126d2be0] none}:
 
-	// JSON format:
-	js, _ := json.MarshalforHTML(vc)
-	fmt.Printf("JSON format: %s", js)
+	// // JSON format:
+	// js, _ := json.Marshal(vc)
+	// fmt.Printf("JSON format: %s", js)
 
-	// using an encoder:
-	file, _ := os.OpenFile("vcard.json", os.O_CREATE|os.O_WRONLY, 0666)
-	defer file.Close()
-	enc := json.NewEncoder(file)
-	err := enc.Encode(vc)
-	if err != nil {
-		log.Println("Error in encoding json")
+	// var out bytes.Buffer
+	// json.Indent(&out, js, "", "\t")
+	// fmt.Println(out.String())
+	// out.WriteTo(os.Stdout)
+
+	// goodJSON := `true`
+	// badJSON := `{"example":2:]}}`
+
+	// fmt.Println("\n", json.Valid([]byte(goodJSON)), json.Valid([]byte(badJSON)))
+
+	// fmt.Println(io.EOF.Error())
+
+	// // using an encoder:
+	// file, _ := os.OpenFile("vcard.json", os.O_CREATE|os.O_WRONLY, 0666)
+	// defer file.Close()
+	// enc := json.NewEncoder(file)
+	// err := enc.Encode(vc)
+	// if err != nil {
+	// 	log.Println("Error in encoding json")
+	// }
+
+	jsonData := []byte(`{"Name":"Eve","Age":6,"Parents":["Alice","Bob"]}`)
+
+	var v interface{}
+	json.Unmarshal(jsonData, &v)
+	data := v.(map[string]interface{})
+
+	for k, v := range data {
+		switch v := v.(type) {
+		case string:
+			fmt.Println(k, v, "(string)")
+		case float64:
+			fmt.Println(k, v, "(float64)")
+		case []interface{}:
+			fmt.Println(k, "(array):")
+			for i, u := range v {
+				fmt.Println("    ", i, u)
+			}
+		default:
+			fmt.Println(k, v, "(unknown)")
+		}
 	}
 }
