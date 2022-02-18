6e4e072c9f8559ccd8684920e3e5bafa4f272b0c
francis <yinghao_liu@163.com>
2022-02-18 10:53:59 +0800

    duplication of struct and value

--- 005/main.go
+++ 005/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	st "structtest/struct"
 )
 
 // alias
 type BaseAlias st.Base2
+
+//
+type Name struct {
+	Name string
+}
 
 func main() {
 	var a st.BaseInfo
@@ -19,4 +25,9 @@ func main() {
 	// alias
 	//var b BaseAlias
 	//b.show() // b.show undefined (type Base2 has no field or method show)
+
+	var Name Name
+	Name.Name = "Name"
+	fmt.Printf("Name is %+v\n", Name)
+
 }
