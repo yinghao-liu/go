bcfc40253455e8c715dc6c3705249a8325c58059
francis <yinghao_liu@163.com>
2022-04-21 22:57:30 +0800

    interface Comparison operation

--- 003/main.go
+++ 003/main.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Square struct {
-	side float32
+	Side float32
 }
 
 func (sq *Square) Area() float32 {
-	return sq.side * sq.side
+	return sq.Side * sq.Side
 }
 
 type Circle struct {
@@ -29,14 +29,32 @@ func typeTest() {
 	var any interface{}
 
 	fmt.Printf("'any' is [%T]-[%T]-[%p]-[%p]\n", any, &any, &any, any)
+	var any2 interface{}
+	if any == any2 {
+		fmt.Printf("1---   any == any2: equal\n")
+	}
 
 	sq1 := new(Square)
 	any = sq1
 	fmt.Printf("'any' is [%T]-[%T]-[%p]-[%p]\n", any, &any, &any, any)
 
 	var sq2 Square
-	any = sq2
-	fmt.Printf("'any' is [%T]-[%p]-[%p]\n", any, &any, any)
+	any2 = sq2
+	fmt.Printf("'any2' is [%T]-[%p]-[%p]\n", any2, &any2, any2)
+
+	if any == any2 {
+		fmt.Printf("2---   any == any2: equal\n")
+	}
+
+	var any3 interface{}
+	var sq3 Square
+	sq3.Side = 111 // without this line, any2 and any3 is equal
+	any3 = sq3
+	fmt.Printf("'any2' is [%T]-[%p]-[%p]\n", any2, &any2, any2)
+	fmt.Printf("'any3' is [%T]-[%p]-[%p]\n", any3, &any3, any3)
+	if any2 == any3 {
+		fmt.Printf("3---   any2 == any3: equal\n")
+	}
 
 	// t and any are not same value, but point to the same address, if ok is true
 	// test if any(*Square) suit interface Shaper, if true, return *Square
